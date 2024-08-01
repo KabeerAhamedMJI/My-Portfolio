278bcf95d69045c7211d3dbebf61a6324bad7279
Kabeer Ahamed <kabeerahamedkm@gmail.com>
2024-08-01 18:56:36 +0530

    Updated new design for skills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,148 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const skills = () => {
+const Skills = () => {
+    const [activeTab, setActiveTab] = useState('tab1');
+
+    // Update the handleTabChange function to return a function
+    const handleTabChange = (tab) => {
+        return () => setActiveTab(tab);
+    };
+
     return (
         <section>
-            <div className='containers'>
+            <div className='container mx-auto px-4 py-12'>
                 <div className='flex flex-col items-center pb-12'>
-                    <span className='text-center mb-4' style={{ color: '#ff014f' }}>
+                    <span className='text-center mb-4 text-red-600'>
                         Technical Skills for Modern Web
                     </span>
-                    <h2 id='skills' className='text-center text-2xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl font-bold' style={{ color: '#BEBEBE' }}>
+                    <h2 id='skills' className='text-center text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-gray-300'>
                         Proficiencies
                     </h2>
                 </div>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8'>
-                    <div className='skills'>
-                        <h3></h3>
-                        <div className='flex flex-row items-center space-x-4'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '90%' }}></div>
-                            </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '80%' }}></div>
-                            </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '95%' }}></div>
-                            </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '20%' }}></div>
-                            </div>
-                        </div>
+                <div className='tab-container'>
+                    <div className='flex mb-4'>
+                        <button id='skillButton'
+                            className={`px-8 py-4 text-lg font-semibold ${activeTab === 'tab1' ? ' text-red-600 rounded-box p-6' : 'text-gray-300'} focus:outline-none`}
+                            onClick={handleTabChange('tab1')}
+                        >
+                            Frontend Technologies
+                        </button>
+                        <button  id='skillButton'
+                            className={`px-8 py-4 text-lg font-semibold ${activeTab === 'tab2' ? ' text-red-600 rounded-box p-6' : 'text-gray-300'} focus:outline-none`}
+                            onClick={handleTabChange('tab2')}
+                        >
+                           Backend Technologies
+                        </button>
+                        <button  id='skillButton'
+                            className={`px-8 py-4 text-lg font-semibold ${activeTab === 'tab3' ? ' text-red-600 rounded-box p-6' : 'text-gray-300'} focus:outline-none`}
+                            onClick={handleTabChange('tab3')}
+                        >
+                          Development Tools
+                        </button>
+                        <button  id='skillButton'
+                            className={`px-8 py-4 text-lg font-semibold ${activeTab === 'tab4' ? ' text-red-600 rounded-box p-6' : 'text-gray-300'} focus:outline-none`}
+                            onClick={handleTabChange('tab4')}
+                        >
+                          Proffessional Experience
+                        </button>
                     </div>
-                    <div className='skills'>
-                        <div className='flex flex-row items-center space-x-4'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '90%' }}></div>
+                    <div className='card-container'>
+                        {activeTab === 'tab1' && (
+                            <div  id='dataBox' className='rounded-lg p-6'>
+                                <h3 className='text-xl font-semibold mb-4'>Frontend Technologies</h3>
+                                <p className='text-gray-700'>List your frontend skills here, such as React, Tailwind CSS, etc.</p>
                             </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '80%' }}></div>
+                        )}
+                        {activeTab === 'tab2' && (
+                            <div id='dataBox' className='rounded-lg p-6'>
+                                <h3 className='text-xl font-semibold mb-4'>Backend Technologies</h3>
+                                <p className='text-gray-700'>List your backend skills here, such as Node.js, Express, etc.</p>
                             </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '95%' }}></div>
+                        )}
+                        {activeTab === 'tab3' && (
+                            <div  id='dataBox' className='rounded-lg p-6'>
+                                <h3 className='text-xl font-semibold mb-4'>Development Tools</h3>
+                                <p className='text-gray-700'>List your tools and other technologies here, such as Git, Docker, etc.</p>
                             </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '20%' }}></div>
+                        )}
+                                                {activeTab === 'tab4' && (
+                            <div  id='dataBox' className='rounded-lg p-6'>
+                                <h3 className='text-xl font-semibold mb-4'>Proffessional Experience</h3>
+                                <p className='text-gray-700'>List your tools and other technologies here, such as Git, Docker, etc.</p>
                             </div>
-                        </div>
-                    </div><div className='skills'>
-                        <div className='flex flex-row items-center space-x-4'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '90%' }}></div>
-                            </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '80%' }}></div>
-                            </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '95%' }}></div>
-                            </div>
-                        </div>
-                        <div className='flex flex-row items-center space-x-4 mt-6'>
-                            <img
-                                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/badge-html-5-256.png"
-                                alt="HTML5 Badge"
-                                className="w-8 h-8 md:w-12 md:h-12"
-                            />
-                            <div className="w-full bg-gray-200 rounded-full h-4">
-                                <div className="bg-blue-500 h-4 rounded-full" style={{ width: '20%' }}></div>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default skills
+export default Skills;
